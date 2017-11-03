cb28074a7c0666bec0faabdcbecea27f0a12e302
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-11-03 11:46:36 +0100

    Require fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 # Default CarrierWave setup.
 #
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   config.permissions = 0o666
   config.directory_permissions = 0o777
