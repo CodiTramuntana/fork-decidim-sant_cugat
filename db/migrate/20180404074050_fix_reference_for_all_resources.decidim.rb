65c1e65765b5088ed10b97c50365caa973155cdf
Oliver Valls <oliver.vh@coditramuntana.com>
2020-02-04 13:11:56 +0100

    Fix migrations to work in development environment.

--- db/migrate/20180404074050_fix_reference_for_all_resources.decidim.rb
+++ db/migrate/20180404074050_fix_reference_for_all_resources.decidim.rb
@@ -6,8 +6,13 @@ class FixReferenceForAllResources < ActiveRecord::Migration[5.1]
     models = ActiveRecord::Base.descendants.select { |c| c.included_modules.include?(Decidim::HasReference) }
 
     models.each do |model|
+      put "model: #{model}"
       model.find_each(&:touch)
+      puts "."
+    rescue Exception => e
+      Rails.logger.warn "could not touch #{model}: #{e}"
     end
+    puts "DONE."
   end
 
   def down; end
